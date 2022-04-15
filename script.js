7277f39c7bc021f6b54b6d275bbdde03bb29a69b
Hai Network <hai.network@protonmail.com>
2022-04-15 18:23:46 +0200

    ao

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let qr=document.getElementById('qr')
-qr.onclick=()=>{window.open("https://google.com", "_new")}
+qr.onclick=()=>{window.open("/qr/qr.html","_self")}
 
 let qr2=document.getElementById('qr2')
-qr2.onclick=()=>{window.open("https://google.com", "_new")}
+qr2.onclick=()=>{window.open("#","_self")}
