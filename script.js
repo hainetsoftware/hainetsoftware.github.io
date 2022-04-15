22f5722e6e2ebd2d9cde7749776897a63c09a8d8
Hai Network <hai.network@protonmail.com>
2022-04-15 21:54:52 +0200

    big changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let qr=document.getElementById('qr')
 qr.onclick=()=>{window.open("/qr/qr.html","_self")}
 
-let qr2=document.getElementById('qr2')
-qr2.onclick=()=>{window.open("#","_self")}
+let qr2=document.getElementById('bbsky')
+qr2.onclick=()=>{window.open("blue-blue-sky/bbsky.html","_self")}
