f6a800bd2e7c0b655e8b0f1253628c1251a1214d
Hai Network <hai.network@protonmail.com>
2022-04-15 18:41:14 +0200

    stop war

--- qr/script.js
+++ qr/script.js
@@ -8,7 +8,7 @@ generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value.trim();
     if(!qrValue || preValue === qrValue) return;
     preValue = qrValue;
-    generateBtn.innerText = "Ci sto lavorando...";
+    generateBtn.innerText = "Generating QR Code...";
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=268x268&data=${qrValue}`;
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
