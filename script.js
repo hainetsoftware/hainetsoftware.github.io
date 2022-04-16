304f9d1b722af8b53d26b27905ce0522c53e821f
Hai Network <hai.network@protonmail.com>
2022-04-16 14:03:58 +0200

    see 'snapshot 220416.1' for info (github button need to be fixed!)

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ qr.onclick=()=>{window.open("/qr/qr.html","_self")}
 
 let qr2=document.getElementById('bbsky')
 qr2.onclick=()=>{window.open("blue-blue-sky/bbsky.html","_self")}
+
+let cmngsn=document.getElementById('cmngsn')
+cmngsn.onclick=()=>{window.open("https://www.youtube.com/watch?v=LHQqqM5sr7g","_new")}
+
+let cmngsn2=document.getElementById('cmngsn2')
+cmngsn2.onclick=()=>{window.open("https://www.youtube.com/watch?v=LHQqqM5sr7g","_new")}
