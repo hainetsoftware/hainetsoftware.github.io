38bafe8245d1965a8d88cefd036d3f90167c187e
Hai Network <hai.network@protonmail.com>
2022-04-18 01:10:28 +0200

    new site !!! all is ready for the first version of the native app

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ qr.onclick=()=>{window.open("/qr/qr.html","_self")}
 let qr2=document.getElementById('bbsky')
 qr2.onclick=()=>{window.open("blue-blue-sky/bbsky.html","_self")}
 
-let cmngsn=document.getElementById('cmngsn')
-cmngsn.onclick=()=>{window.open("https://www.youtube.com/watch?v=LHQqqM5sr7g","_new")}
+let wb=document.getElementById('wb')
+wb.onclick=()=>{window.open("what-browser/wb.html","_self")}
 
 let cmngsn2=document.getElementById('cmngsn2')
 cmngsn2.onclick=()=>{window.open("https://www.youtube.com/watch?v=LHQqqM5sr7g","_new")}
